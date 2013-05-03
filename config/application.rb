e48608e1e3697e788f90ce1e2c9a17ea6f0e8ff1
Faun <faun@riseup.net>
2013-05-03 15:11:21 -0700

    Add generator defaults

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ end
 
 module DistroNew
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      g.helper false
+      g.template_engine :slim
+      g.stylesheets false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
