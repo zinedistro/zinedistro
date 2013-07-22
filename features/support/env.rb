a14189162c903b7c0b7b85850b11e49339dc9621
Faun <faun@riseup.net>
2013-07-21 22:07:08 -0700

    Only require SimpleCov if ENV['COVERAGE']

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@
 
 ENV["RAILS_ENV"] ||= 'test'
 require 'cucumber/rails'
-require 'simplecov'
+require 'simplecov' if ENV['COVERAGE']
 
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require 'simplecov'
+require 'simplecov' if ENV['COVERAGE']
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -36,3 +36,28 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
+
