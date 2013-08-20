6d5f7e2a80db3c6c8d06476ed59d19f6e7c41b20
Faun <faun@riseup.net>
2013-08-19 18:06:12 -0700

    Add author with zines trait

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -2,4 +2,9 @@ FactoryGirl.define do
   factory :author do
     sequence(:name) { |n| "Author #{n}" }
   end
+  trait :with_zines do
+    after(:create) do |instance|
+      create_list :zine, 3, authors: [instance]
+    end
+  end
 end
