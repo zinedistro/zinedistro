f3193c3434d30355d6c9804ed13306fedcf2d4dc
Faun <faun@riseup.net>
2013-12-02 22:14:43 +0700

    Move SECRET_TOKEN & SECRET_KEY_BASE to .env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,11 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-if Rails.env.test? || Rails.env.development? || Rails.env == "profile"
-  DistroNew::Application.config.secret_token = "k4ycDncmDors9PzaBjE72Q9NiLuQu7Bq3Lxw28P8VRMCEfPEuEEW23AUF7yj9iTcmApWVqeZzzMGBCKpn9QJ7YyAZoFuq7pm873uww9Zcf9xJ33ppXFuGvqgLyWhz99CZFfx8vW6TjqCLyJ8BF6HfP"
-  DistroNew::Application.config.secret_key_base = DistroNew::Application.config.secret_token
-else
-  raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
-  DistroNew::Application.config.secret_token = ENV['SECRET_TOKEN']
-  DistroNew::Application.config.secret_key_base = DistroNew::Application.config.secret_token
-end
+fail 'You must set a secret token value' if ENV['SECRET_TOKEN'].blank?
+DistroNew::Application.config.secret_token = ENV['SECRET_TOKEN']
+
+fail 'You must set a secret key base value' if ENV['SECRET_KEY_BASE'].blank?
+DistroNew::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
