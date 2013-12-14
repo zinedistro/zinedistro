8bd8fea378be54d229d68c75e3bbb97dc3a75d2e
Faun <faun@riseup.net>
2013-12-14 21:29:51 +0700

    Add English as the default locale

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module ZineDistro
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    I18n.enforce_available_locales = true
+    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :en
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
