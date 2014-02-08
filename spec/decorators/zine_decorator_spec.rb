566b00f6eb3b7ff94942615ee6858b7e655e05b4
Faun <faun@riseup.net>
2014-02-08 11:16:43 +0700

    Clean up decorator spec

--- spec/decorators/zine_decorator_spec.rb
+++ spec/decorators/zine_decorator_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe ZineDecorator do
+  let(:zine) { create :zine_with_authors }
+  subject do
+    described_class.new(zine)
+  end
   describe '#link_to_cover_image_tag' do
     context 'with a cover image' do
       subject do
@@ -19,44 +23,35 @@ describe ZineDecorator do
       end
       it 'returns the default image' do
         subject.link_to_cover_image_tag
-        .should eq '<img alt="Missing zine cover" ' +
+        .should eq '<img alt="Missing zine cover" ' \
         'src="http://assets.zinedistro.org/zines/covers/missing.png" />'
       end
     end
   end
 
   describe '#path' do
-    subject do
-      described_class.new(create :zine_with_authors)
-    end
-
     it 'returns a sanitized, seo-friendly url string' do
       subject.path
-        .should eq '/zines/' +
-        "#{subject.object.id}/" +
-        "#{subject.object.title.downcase.gsub(' ', '-')}/" +
-        "by/author-#{subject.object.authors[0].id}" +
-        "-author-#{subject.object.authors[1].id}-" +
-        "and-author-#{subject.object.authors[2].id}"
+        .should eq '/zines/' \
+        "#{zine.id}/" \
+        "#{zine.title.downcase.gsub(' ', '-')}/" \
+        "by/author-#{zine.authors[0].id}" \
+        "-author-#{zine.authors[1].id}-" \
+        "and-author-#{zine.authors[2].id}"
     end
   end
 
   describe '#author_names' do
-    subject do
-      described_class.new(zine)
-    end
-
     context 'with multiple authors' do
-      let(:zine) { create :zine_with_authors }
       it 'returns a concatenated string of authors' do
-        expect(subject.author_names).to eq "#{zine.authors[0].name}, " +
-          "#{zine.authors[1].name}, " +
+        expect(subject.author_names).to eq "#{zine.authors[0].name}, " \
+          "#{zine.authors[1].name}, " \
           "and #{zine.authors[2].name}"
       end
     end
 
     context 'whithout an author' do
-      let(:zine) { create :zine }
+      let(:zine) { build :zine }
       it "returns 'anonymous'" do
         expect(subject.author_names).to eq 'Anonymous'
       end
