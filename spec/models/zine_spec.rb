69aab860bc78f0c2cd02fc3cd7ae9845977c7eef
Faun <faun@riseup.net>
2014-05-06 16:19:53 +0700

    Move let blocks into context and clean up

--- spec/models/zine_spec.rb
+++ spec/models/zine_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
-describe Zine do
 
-  let(:published_zine) { create :zine_with_authors }
-  let(:unpublished_zine) { create :zine_with_authors, :unpublished }
-  let(:zine) { create :zine }
-  let(:author) { create :author }
-  let(:last_author) { published_zine.authors.last }
+describe Zine do
 
   describe 'validations' do
     context 'without a cover image' do
-      let(:zine) { build(:zine, cover_image: nil) }
+      let(:zine) { build :zine, cover_image: nil }
+
       it 'is not valid' do
         expect(zine).to_not be_valid
         expect(zine.errors.count).to equal 1
@@ -21,7 +17,9 @@ describe Zine do
   end
 
   describe '.find_published' do
-    context 'a published zine' do
+    context 'with a published zine' do
+      let(:published_zine) { create :zine_with_authors }
+
       it 'returns the published zine' do
         described_class.find_published(published_zine.id)
         .should eq published_zine
@@ -29,6 +27,8 @@ describe Zine do
     end
 
     context 'with an unpublished zine' do
+      let(:unpublished_zine) { build :zine_with_authors, :unpublished }
+
       it 'raises "record not found"' do
         expect {
           described_class.find_published(unpublished_zine.id)
@@ -37,6 +37,7 @@ describe Zine do
     end
 
     context 'without a zine' do
+
       it 'raises "record not found"' do
         expect {
           described_class.find_published('robots')
@@ -46,79 +47,93 @@ describe Zine do
   end
 
   describe '.catalog' do
-    it 'returns published zines' do
-      described_class.catalog.should include published_zine
-    end
+    context 'with published a zine' do
+      let(:published_zine) { create :zine_with_authors }
+      let(:unpublished_zine) { build :zine_with_authors, :unpublished }
 
-    it "doesn't return unpublished zines" do
-      described_class.catalog.should_not include unpublished_zine
+      it 'returns published zines' do
+        described_class.catalog.should include published_zine
+      end
+
+      it "doesn't return unpublished zines" do
+        described_class.catalog.should_not include unpublished_zine
+      end
     end
   end
 
   describe '#add_author' do
-    it 'adds an author as one of the zine authors' do
-      expect(zine.authors).to eq []
-      zine.add_author(author)
-      expect(zine.reload.authors).to eq [author]
-    end
+    context "with an author" do
+      let(:author) { build :author }
+      let(:zine) { create :zine }
 
-    it 'does not add the author multiple times' do
-      zine.author_count.should eq 0
-      zine.add_author(author)
-      expect {
+      it 'adds an author as one of the zine authors' do
+        expect(zine.authors).to eq []
         zine.add_author(author)
-      }.to raise_error(ActiveRecord::RecordInvalid)
-      zine.reload.author_count.should eq 1
+        expect(zine.authors).to eq [author]
+      end
+
+      it 'does not add the author multiple times' do
+        zine.author_count.should eq 0
+        zine.add_author(author)
+        expect {
+          zine.add_author(author)
+        }.to raise_error(ActiveRecord::RecordInvalid)
+        zine.author_count.should eq 1
+      end
     end
   end
 
   describe '#remove_author' do
     context 'with an author' do
+      let(:last_author) { published_zine.authors.last }
+      let(:published_zine) { create :zine_with_authors }
+
       it 'removes the author as one of the zine authors' do
         expect(published_zine.remove_author(last_author)).to be_truthy
-        expect(published_zine.reload.author_count).to eq 2
+        expect(published_zine.author_count).to eq 2
       end
     end
   end
 
   describe '#author_count' do
-    context 'with authors' do
+    context 'with published zines' do
+      let(:published_zine) { create :zine_with_authors }
+      let(:zine) { create :zine }
+      let(:author) { build :author }
+
       it 'returns the number of authors for a given zine' do
-        published_zine.reload.author_count.should eq 3
+        published_zine.author_count.should eq 3
       end
-    end
 
-    context 'with no authors' do
-      it 'returns 0' do
-        zine.reload.author_count.should eq 0
+      context 'when adding an author' do
+        it 'incriments author_count' do
+          expect {
+            zine.add_author(author)
+          }.to change(zine.authors, :count).by(1)
+        end
       end
-    end
 
-    context 'when adding an author' do
-      it 'incriments author_count' do
-        expect {
-          zine.add_author(author)
-        }.to change(zine.reload.authors, :count).by(1)
-      end
-    end
-
-    context 'when removing an author' do
-      it 'decrements author_count' do
-        zine.authors << author
-        expect {
-          zine.remove_author(author)
-        }.to change(zine.reload, :author_count).by(-1)
+      context 'when removing an author' do
+        it 'decrements author_count' do
+          zine.authors << author
+          expect {
+            zine.remove_author(author)
+          }.to change(zine, :author_count).by(-1)
+        end
       end
     end
   end
 
   describe '#publish!' do
-    let(:zine) { build :zine, :unpublished }
-    it 'changes the published attribute' do
-      zine.published.should be false
-      expect {
-        zine.publish!
-      }.to change(zine, :published).from(false).to(true)
+    context 'with unpublished zines' do
+      let(:zine) { build :zine, :unpublished }
+      it 'changes the published attribute' do
+        zine.published.should be false
+        expect {
+          zine.publish!
+        }.to change(zine, :published).from(false).to(true)
+      end
     end
   end
+
 end
