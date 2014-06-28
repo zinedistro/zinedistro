730597eea2dd25578f2ad89bb055563ea17da3c6
Faun <faun@riseup.net>
2014-06-28 21:23:09 +0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603041645) do
+ActiveRecord::Schema.define(version: 20140622053608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20140603041645) do
     t.datetime "added_at"
     t.integer  "author_count", default: 0,    null: false
     t.string   "pdf"
+    t.integer  "legacy_id"
   end
 
 end
