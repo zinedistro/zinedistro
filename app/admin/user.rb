6652b606f8a5813ac9c1c905426d98b564cddd50
Faun <faun@riseup.net>
2014-08-01 15:41:43 +0700

    Add user creation admin interface

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,20 @@
 ActiveAdmin.register User do
+  controller do
+    def permitted_params
+      params.permit(:utf8,
+                    :commit,
+                    :authenticity_token,
+                    user: [:email,
+                           :password,
+                           :password_confirmation])
+    end
+  end
+
   index do
     column :email
     column :current_sign_in_at
     column :last_sign_in_at
     column :sign_in_count
-    default_actions
   end
 
   filter :email
---
+++ spec/features/user_creation_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+require 'carrierwave/test/matchers'
+feature 'As a user' do
+  before do
+    sign_in
+  end
+
+  scenario 'I can add a new user' do
+
+    visit new_admin_user_path
+    fill_in 'user_email', with: "user@zinedistro.org"
+    fill_in 'user_password', with: "a password"
+    fill_in 'user_password_confirmation', with: "a password"
+    click_button 'Create User'
+    expect(page).to have_content success_message_for :user
+  end
+end
