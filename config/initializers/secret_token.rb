7fb704dd6dc5cd4d51afd57b38eba42b007187a6
Faun <faun@riseup.net>
2014-09-26 14:35:29 -0700

    Remove crufty Rails 3 secret_token configuration

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-fail 'You must set a secret token value' if ENV['SECRET_TOKEN'].blank?
-ZineDistro::Application.config.secret_token = ENV['SECRET_TOKEN']
 
 fail 'You must set a secret key base value' if ENV['SECRET_KEY_BASE'].blank?
 ZineDistro::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
