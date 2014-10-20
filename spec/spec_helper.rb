596eaa6a5603b0ac4f4bb0a746806c625065ec39
Faun <faun@riseup.net>
2014-10-19 17:06:32 -0700

    Move SimpleCov config into spec helper
    Otherwise it is triggered on load for other rake tasks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,24 @@
 require 'webmock/rspec'
+binding.pry
 if ENV['COVERAGE'] || ENV['CI']
   require 'simplecov'
-  SimpleCov.start 'rails'
+  SimpleCov.start 'rails' do
+    add_filter '/spec/'
+    add_filter '/config/'
+    add_filter '/lib/'
+    add_filter '/vendor/'
+
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Views', 'app/views'
+    add_group 'Decorators', 'app/decorators'
+    add_group 'ActiveAdmin', 'app/admin'
+    add_group 'Uploaders', 'app/uploaders'
+
+    minimum_coverage 100
+  end
 end
 if ENV['CI']
   require "codeclimate-test-reporter"
