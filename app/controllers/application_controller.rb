2a0ef91dcd01f593648e287c4059995602be4b34
Faun <github@faun.me>
2015-01-09 16:46:51 -0800

    Move handler for :not_found message to a method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 # ApplicationController
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  rescue_from ActiveRecord::RecordNotFound do
-    flash[:notice] = I18n.t('flash.record_not_found')
-    not_found
-  end
-
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   def not_found
+    flash[:notice] = I18n.t('flash.record_not_found')
     render(file: 'application/_missing',
            status: :not_found,
            formats: [:html],
