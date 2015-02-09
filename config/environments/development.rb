98196f3f801d87fb0711298e7e7f8d65dc087e9c
Faun <github@faun.me>
2015-02-09 14:08:29 -0800

    Raise exceptions for missing translations in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-  
+  config.action_view.raise_on_missing_translations = true
+
   config.after_initialize do
     Bullet.enable = true
     Bullet.rails_logger = true
