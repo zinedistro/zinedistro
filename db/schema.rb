a6e35bc0e0642db01671df0fee7eb8f98f1c8932
Faun <github@faun.me>
2015-02-09 16:25:59 -0800

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -16,20 +16,20 @@ ActiveRecord::Schema.define(version: 20140622053608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authors", force: true do |t|
+  create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "authorships", id: false, force: true do |t|
+  create_table "authorships", id: false, force: :cascade do |t|
     t.integer "author_id", null: false
     t.integer "zine_id",   null: false
   end
 
   add_index "authorships", ["author_id", "zine_id"], name: "index_authorships_on_author_id_and_zine_id", unique: true, using: :btree
 
-  create_table "comments", force: true do |t|
+  create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "zine_id"
     t.text     "content"
@@ -37,14 +37,14 @@ ActiveRecord::Schema.define(version: 20140622053608) do
     t.datetime "updated_at"
   end
 
-  create_table "faves", force: true do |t|
+  create_table "faves", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "zine_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tags", force: true do |t|
+  create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
     t.integer  "zine_id"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20140622053608) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20140622053608) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "zines", force: true do |t|
+  create_table "zines", force: :cascade do |t|
     t.string   "title",                       null: false
     t.string   "subtitle"
     t.datetime "created_at"
