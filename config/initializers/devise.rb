24023ae8436a1b3f0a00c91a10dd8fd26a59a73e
Faun <github@faun.me>
2015-02-12 10:00:36 -0800

    Change default devise email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = "no-reply@#{ENV.fetch('EMAIL_HOST')}"
 
   if ENV['DEVISE_SECRET_KEY'].blank?
     fail <<-EOS
