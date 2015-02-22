98086f0dbdd11afb2857ae9b9a16f23ce4088f15
Faun <github@faun.me>
2015-02-21 22:23:14 -0800

    Raise exceptions for missing translations

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,1 +1,6 @@
 I18n.enforce_available_locales = true
+if Rails.env.development? || Rails.env.test?
+  I18n.exception_handler = lambda do |exception, locale, key, options|
+    raise "missing translation: #{key}"
+  end
+end
