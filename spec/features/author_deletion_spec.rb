b928dfae22d9c0e5b51eb43de966fb5951af88f7
Faun <github@faun.me>
2015-02-22 16:14:37 -0800

    Fix hound issues
    Conflicts:
    	spec/features/author_deletion_spec.rb

--- spec/features/author_deletion_spec.rb
+++ spec/features/author_deletion_spec.rb
@@ -10,7 +10,7 @@ feature "Deleing Authors" do
   scenario "Visiting the author page" do
     visit admin_authors_path
     expect(page).to have_link t("authors.pluralized_title")
-    click_link t('active_admin.delete')
+    click_link t("active_admin.delete")
     expect(page).to have_content destroy_message_for :author
-    end
+  end
 end
