083f992c163b92bba81e6df85bd3d94cf2c60c2f
Faun <github@faun.me>
2015-03-05 14:28:06 -0800

    Check if the Rails constant is available
    Exceptions for missing exceptions should be raised in development mode
    or in CI

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,6 +1,8 @@
 I18n.enforce_available_locales = true
-if Rails.env.development? || Rails.env.test?
-  I18n.exception_handler = lambda do |exception, locale, key, options|
-    raise "missing translation: #{key}"
+if Object.const_defined?(:ZineDistro) && ZineDistro.const_defined?(:Application)
+  if Rails.env.development? || Rails.env.test?
+    I18n.exception_handler = lambda do |_exception, locale, key, _options|
+      fail "missing translation: #{key} for #{locale}"
+    end
   end
 end
