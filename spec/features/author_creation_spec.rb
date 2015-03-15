b18ddfbdd41fae584a95e5ba41c0cfe89b7113b9
Faun <github@faun.me>
2015-03-14 21:26:39 -0700

    Swap spec helper for correct one

--- spec/features/author_creation_spec.rb
+++ spec/features/author_creation_spec.rb
@@ -1,5 +1,5 @@
-require "spec_helper"
-feature "Creating Authors" do
+require 'rails_helper'
+feature 'Creating Authors' do
   let(:user) { create :user }
   let(:author) { attributes_for :author }
 
@@ -7,11 +7,11 @@ feature "Creating Authors" do
     sign_in user
   end
 
-  scenario "Visiting the author page" do
+  scenario 'Visiting the author page' do
     visit new_admin_author_path
-    expect(page).to have_link t("authors.pluralized_title")
-    fill_in "author_name", with: author
-    click_button "Create Author"
+    expect(page).to have_link t('authors.pluralized_title')
+    fill_in 'author_name', with: author
+    click_button 'Create Author'
     expect(page).to have_content create_message_for :author
   end
 end
