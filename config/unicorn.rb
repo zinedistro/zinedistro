25889a161fe8b1eb5be275cc292f08bd5643d97e
Faun <github@faun.me>
2015-04-12 16:05:37 -0700

    Send info messages about Unicorn status to logger

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ preload_app true
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    logger.info 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -20,7 +20,7 @@ end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    logger.info 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
