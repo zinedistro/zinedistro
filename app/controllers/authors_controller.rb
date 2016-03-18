06ca4f5c156d0a9cfb2ba9099fb1aed420684894
Faun <github@faun.me>
2016-03-18 08:27:55 -0700

    Add Etag support

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,11 +2,20 @@
 class AuthorsController < ApplicationController
   def index
     @authors = Author.published.order_by_name
-    fresh_when last_modified: @authors.maximum(:updated_at), public: true
+    @most_recent = Author.most_recently_updated_at
+    fresh_when(
+      last_modified: @most_recent,
+      public: true,
+      etag: @most_recent
+    )
   end
 
   def show
     @author = Author.find_published(params[:id])
-    fresh_when last_modified: @author.updated_at, public: true
+    fresh_when(
+      last_modified: @author.updated_at,
+      public: true,
+      etag: @author.cache_key
+    )
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -6,19 +6,31 @@ class Author < ActiveRecord::Base
   has_many :zines, through: :authorships
 
   scope :published, -> {
-    joins(:zines)
-      .merge(Zine.published)
-      .distinct('authors.id')
+    joins(:zines).
+    merge(Zine.published).
+    distinct('authors.id')
   }
 
   scope :order_by_name, -> {
     order('name asc')
   }
 
+  scope :order_by_updated, lambda {
+    order(updated_at: :desc)
+  }
+
+  scope :most_recently_updated, lambda {
+    order_by_updated.limit(1)
+  }
+
+  def self.most_recently_updated_at
+    most_recently_updated.pluck(:updated_at).first
+  end
+
   def self.find_published(id)
-    published
-      .where(id: id.to_i)
-      .first || raise(ActiveRecord::RecordNotFound)
+    published.
+      where(id: id.to_i).
+      first || raise(ActiveRecord::RecordNotFound)
   end
 
   scope :with_authors, -> { includes(:authors) }
