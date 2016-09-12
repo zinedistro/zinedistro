2aba694f7379ee3448401b431cde03a78ac2d319
Faun <github@faun.me>
2016-09-11 22:18:31 -0700

    Add Etag support (#116)

--- app/controllers/zines_controller.rb
+++ app/controllers/zines_controller.rb
@@ -3,13 +3,21 @@ class ZinesController < ApplicationController
   def index
     @zines = Zine.catalog_with_authors.page(ZineParams.index(params)[:page]).decorate
     @most_recent = Zine.most_recently_published_date
-    fresh_when last_modified: @most_recent, public: true
+    fresh_when(
+      last_modified: @most_recent,
+      public: true,
+      etag: @most_recent
+    )
   end
 
   def show
     @zine = Zine.find_published_with_authors(ZineParams.show(params)).decorate
     not_found unless @zine
-    fresh_when last_modified: @zine.updated_at, public: true
+    fresh_when(
+      last_modified: @zine.updated_at,
+      public: true,
+      etag: @zine.cache_key
+    )
   end
 
   # strong parameters for zine
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,23 +1,51 @@
 require 'rails_helper'
 require_dependency 'author'
+include ActiveSupport::Testing::TimeHelpers
 
 describe Author do
   describe 'find_published' do
     context 'with a publshed author' do
       let(:author) { FactoryGirl.create :author_with_zines }
+
       before do
         author.zines.last.publish!
       end
+
       it 'returns the published author' do
         expect(described_class.find_published(author.id)).to eq(author)
       end
     end
+
     context 'without an author' do
       let(:not_found) { ActiveRecord::RecordNotFound }
+
       it 'raises not_found' do
         expect {
           described_class.find_published(0)
         }.to raise_error not_found
+      end
+    end
+
+    describe '#most_recent' do
+      let(:author) { FactoryGirl.create :author_with_zines }
+
+      before do
+        travel_to Time.zone.local(2004, 11, 24, 01, 04, 44)
+        author
+      end
+
+      after do
+        travel_back
+      end
+
+      it 'returns a time with zone object' do
+        expect(Author.most_recently_updated_at.class.to_s).to(
+          eq 'ActiveSupport::TimeWithZone'
+        )
+
+        expect(Author.most_recently_updated_at.to_s).to(
+          eq('2004-11-24 01:04:44 -0800')
+        )
       end
     end
   end
