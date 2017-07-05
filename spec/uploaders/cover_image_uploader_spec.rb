452b0cbb13fe6b52f519d14cbdd1f84e58ed4290
Faun <github@faun.me>
2017-07-05 16:58:45 -0700

    Improve uploader test coverage

--- spec/uploaders/cover_image_uploader_spec.rb
+++ spec/uploaders/cover_image_uploader_spec.rb
@@ -6,17 +6,49 @@ describe CoverImageUploader do
     File.open(File.join(Rails.root, '/spec/fixtures/images/transparent.png'))
   end
 
-  let(:cover_image) { described_class.new }
+  let(:instance) { described_class.new }
 
   before do
-    allow(cover_image).to receive_message_chain(:model, :id)
+    CoverImageUploader.enable_processing = true
+    allow(instance).to receive_message_chain(:model, :id)
       .and_return(::SecureRandom.hex)
-    cover_image.store! fixture_image
+    instance.store! fixture_image
+  end
+
+  after do
+    CoverImageUploader.enable_processing = false
+    instance.remove!
   end
 
   context 'test env' do
     it 'uploads the cover image to the correct bucket' do
-      expect(cover_image.file.url).to match(/^#{ENV['CARRIERWAVE_ASSET_HOST']}/)
+      expect(instance.file.url).to match(/^#{ENV['CARRIERWAVE_ASSET_HOST']}/)
+    end
+
+    it "allows the image to be converted to jpg" do
+      instance.convert(:jpg)
+      expect(instance.file.extension).to eq('jpg')
+    end
+
+    it "allows the images to be converted to png" do
+      instance.convert(:png)
+      expect(instance.file.extension).to eq('png')
+    end
+
+    it "allows the images to be converted to png" do
+      instance.convert(:gif)
+      expect(instance.file.extension).to eq('gif')
+    end
+
+    it "allows the images to be converted to jpeg" do
+      instance.convert(:jpeg)
+      expect(instance.file.extension).to eq('jpeg')
+    end
+
+    it "does not allow the images to be converted to PDF" do
+      expect {
+        instance.convert(:pdf)
+      }.to raise_error { CarrierWave::ProcessingError }
     end
   end
 end
---
+++ spec/uploaders/pdf_uploader_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe PdfUploader do
+
+  def fixture_image
+    File.open(File.join(Rails.root, '/spec/fixtures/pdfs/transparent.pdf'))
+  end
+
+  let(:instance) { described_class.new }
+
+  before do
+    PdfUploader.enable_processing = true
+    allow(instance).to receive_message_chain(:model, :id)
+      .and_return(::SecureRandom.hex)
+    instance.store! fixture_image
+  end
+  
+  after do
+    PdfUploader.enable_processing = false
+    instance.remove!
+  end
+
+  context 'test env' do
+    it 'uploads the cover image to the correct bucket' do
+      expect(instance.file.url).to match(/^#{ENV['CARRIERWAVE_ASSET_HOST']}/)
+    end
+
+    it "has the correct file extension" do
+      expect(instance.file.extension).to eq('pdf')
+    end
+
+    it "does not allow the PDF to be converted" do
+      expect {
+        instance.convert(:pdf)
+      }.to raise_error { CarrierWave::ProcessingError }
+    end
+  end
+end
